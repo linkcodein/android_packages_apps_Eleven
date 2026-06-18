641d7b5cb7fae0884ca32aba7eb17d8765ab971c
hadad <hadad@linuxmail.org>
2026-06-18 03:23:16 +0000

    Eleven: Use BaseFragment for search to fix touch and lifecycle handling

--- src/org/lineageos/eleven/ui/fragments/SearchFragment.java
+++ src/org/lineageos/eleven/ui/fragments/SearchFragment.java
@@ -15,15 +15,11 @@
  */
 package org.lineageos.eleven.ui.fragments;
 
-import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.TypedValue;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -31,9 +27,6 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 import androidx.loader.app.LoaderManager;
 import androidx.loader.content.Loader;
 import androidx.recyclerview.widget.DefaultItemAnimator;
@@ -41,25 +34,19 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import org.lineageos.eleven.Config;
-import org.lineageos.eleven.MusicStateListener;
 import org.lineageos.eleven.R;
 import org.lineageos.eleven.adapters.SongListAdapter;
 import org.lineageos.eleven.loaders.SearchLoader;
 import org.lineageos.eleven.model.Song;
 import org.lineageos.eleven.service.MusicPlaybackTrack;
-import org.lineageos.eleven.ui.activities.BaseActivity;
-import org.lineageos.eleven.ui.activities.HomeActivity;
 import org.lineageos.eleven.utils.MusicUtils;
-import org.lineageos.eleven.utils.PopupMenuHelper;
-import org.lineageos.eleven.utils.SongPopupMenuHelper;
 import org.lineageos.eleven.widgets.LoadingEmptyContainer;
 import org.lineageos.eleven.widgets.NoResultsContainer;
 
 import java.util.List;
-import java.util.TreeSet;
 
-public class SearchFragment extends Fragment implements
-        LoaderManager.LoaderCallbacks<List<Song>>, MusicStateListener {
+public class SearchFragment extends BaseFragment implements
+        LoaderManager.LoaderCallbacks<List<Song>> {
 
     private static final int SEARCH_LOADER = 0;
     private static final long SEARCH_DELAY_MS = 400;
@@ -68,70 +55,35 @@ public class SearchFragment extends Fragment implements
     private SongListAdapter mAdapter;
     private androidx.appcompat.widget.SearchView mSearchView;
     private LoadingEmptyContainer mLoadingEmptyContainer;
-    private PopupMenuHelper mPopupMenuHelper;
     private Handler mSearchHandler;
     private String mLastQuery;
     private List<Song> mSearchedSongs;
-
-    private final Runnable mSearchRunnable = () -> startSearch(mSearchView.getQuery().toString());
 
     @Override
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
 
-        mPopupMenuHelper = new SongPopupMenuHelper(getActivity(), getChildFragmentManager()) {
-            @Override
-            public Song getSong(int position) {
-                return mAdapter.getItem(position);
-            }
-
-            @Override
-            protected long getSourceId() {
-                return -1;
-            }
-
-            @Override
-            protected Config.IdType getSourceType() {
-                return Config.IdType.NA;
-            }
-
-            @Override
-            protected void updateMenuIds(PopupMenuType type, TreeSet<Integer> set) {
-                super.updateMenuIds(type, set);
-            }
-        };
-
         mAdapter = createAdapter();
-        mAdapter.setPopupMenuClickedListener((v, position) ->
-                mPopupMenuHelper.showPopupMenu(v, position));
         mSearchHandler = new Handler(Looper.getMainLooper());
     }
 
     @Override
-    public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
-                             final Bundle savedInstanceState) {
-        final View rootView = inflater.inflate(R.layout.search_fragment, container, false);
+    public void onViewCreated() {
+        super.onViewCreated();
 
-        final Context context = getContext();
-        if (context != null) {
-            TypedValue c = new TypedValue();
-            context.getTheme().resolveAttribute(R.attr.colorSurface, c, true);
-            rootView.setBackgroundColor(c.data);
-        }
-
-        mListView = rootView.findViewById(R.id.list_base);
+        mListView = mRootView.findViewById(R.id.list_base);
         mListView.setAdapter(mAdapter);
-        mListView.setLayoutManager(new LinearLayoutManager(requireActivity()));
+        mListView.setLayoutManager(new LinearLayoutManager(getContainingActivity()));
         mListView.setItemAnimator(new DefaultItemAnimator());
 
-        mLoadingEmptyContainer = rootView.findViewById(R.id.loading_empty_container);
+        mLoadingEmptyContainer = mRootView.findViewById(R.id.loading_empty_container);
         mLoadingEmptyContainer.showLoading();
         mLoadingEmptyContainer.setVisibility(View.VISIBLE);
 
         setupNoResultsContainer(mLoadingEmptyContainer.getNoResultsContainer());
 
-        mSearchView = rootView.findViewById(R.id.search_view);
+        mSearchView = mRootView.findViewById(R.id.search_view);
         mSearchView.setIconified(false);
         mSearchView.requestFocus();
         mSearchView.setOnQueryTextListener(new androidx.appcompat.widget.SearchView.OnQueryTextListener() {
@@ -145,7 +97,7 @@ public class SearchFragment extends Fragment implements
             @Override
             public boolean onQueryTextChange(final String newText) {
                 mSearchHandler.removeCallbacks(mSearchRunnable);
-                if (newText.length() >= 1) {
+                if (newText != null && newText.length() >= 1) {
                     mSearchHandler.postDelayed(mSearchRunnable, SEARCH_DELAY_MS);
                 } else {
                     mLoadingEmptyContainer.showNoResults();
@@ -156,22 +108,17 @@ public class SearchFragment extends Fragment implements
             }
         });
 
-        final Activity activity = getActivity();
-        if (activity instanceof BaseActivity) {
-            ((BaseActivity) activity).setMusicStateListenerListener(this);
-        }
-
-        return rootView;
+        LoaderManager.getInstance(this).initLoader(SEARCH_LOADER, null, this);
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        final Activity activity = getActivity();
-        if (activity instanceof BaseActivity) {
-            ((BaseActivity) activity).removeMusicStateListenerListener(this);
+    private final Runnable mSearchRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (mSearchView != null) {
+                startSearch(mSearchView.getQuery().toString());
+            }
         }
-    }
+    };
 
     @Override
     public void onCreateOptionsMenu(@NonNull final Menu menu,
@@ -204,12 +151,6 @@ public class SearchFragment extends Fragment implements
         LoaderManager.getInstance(this).restartLoader(SEARCH_LOADER, null, this);
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        LoaderManager.getInstance(this).initLoader(SEARCH_LOADER, null, this);
-    }
-
     private void onItemClick(final int position) {
         if (mSearchedSongs == null || mSearchedSongs.isEmpty()) {
             return;
@@ -224,7 +165,7 @@ public class SearchFragment extends Fragment implements
     @NonNull
     @Override
     public Loader<List<Song>> onCreateLoader(final int id, final Bundle args) {
-        return new SearchLoader(getActivity(), mLastQuery);
+        return new SearchLoader(getContainingActivity(), mLastQuery);
     }
 
     @Override
@@ -238,10 +179,8 @@ public class SearchFragment extends Fragment implements
         }
         mSearchedSongs = data;
         mLoadingEmptyContainer.setVisibility(View.GONE);
-        new Handler(requireActivity().getMainLooper()).post(() -> {
-            mAdapter.setData(data);
-            mAdapter.setCurrentlyPlayingTrack(MusicUtils.getCurrentTrack());
-        });
+        mAdapter.setData(data);
+        mAdapter.setCurrentlyPlayingTrack(MusicUtils.getCurrentTrack());
     }
 
     @Override
@@ -251,8 +190,11 @@ public class SearchFragment extends Fragment implements
 
     @Override
     public void onMetaChanged() {
+        super.onMetaChanged();
         final MusicPlaybackTrack currentTrack = MusicUtils.getCurrentTrack();
-        mAdapter.setCurrentlyPlayingTrack(currentTrack);
+        if (currentTrack != null) {
+            mAdapter.setCurrentlyPlayingTrack(currentTrack);
+        }
     }
 
     @Override
@@ -264,6 +206,7 @@ public class SearchFragment extends Fragment implements
 
     @Override
     public void onPlaylistChanged() {
+        super.onPlaylistChanged();
     }
 
     private void setupNoResultsContainer(final NoResultsContainer empty) {
@@ -278,11 +221,26 @@ public class SearchFragment extends Fragment implements
 
     private SongListAdapter createAdapter() {
         return new SongListAdapter(
-                requireActivity(),
+                getContainingActivity(),
                 R.layout.list_item_normal,
                 -1,
                 Config.IdType.NA,
                 this::onItemClick
         );
+    }
+
+    @Override
+    protected String getTitle() {
+        return getString(R.string.menu_search);
+    }
+
+    @Override
+    protected int getLayoutToInflate() {
+        return R.layout.search_fragment;
+    }
+
+    @Override
+    protected boolean needsElevatedActionBar() {
+        return false;
     }
 }
